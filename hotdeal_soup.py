33e08d56f90a215503ad3614d028a2a972ce7f85
Knowho1206 <yuntanx495@gmail.com>
2023-04-04 16:57:15 +0900

    variable modification and major bug fixes

--- hotdeal_soup.py
+++ hotdeal_soup.py
@@ -6,8 +6,8 @@ else:
 	content = response.content #return error message
 
 from bs4 import BeautifulSoup 
-item_lists = [] #contain the info.
-ended_lists = []
+items = [] #contain the info.
+items_ended = []
 
 #finding ALL the items
 soup = BeautifulSoup(content, 'html.parser') #parsing the content
@@ -15,10 +15,15 @@ item_html_list = soup.find_all(class_="vrow hybrid") #1. find all items with cla
 
 for item in item_html_list: #start categorizing
     title = item.find("a", class_="title").get_text().strip() #get the title
+    site_link = item.find("a", class_="title")["href"] #get the link
     price = item.find("span", class_="deal-price").get_text().strip() #get the price
     badge = item.find("a", class_="badge").get_text().strip() #get the badge(category)
-    delPrice = item.find("span", class_="deal-price").get_text().strip() #get the delivery price
-    item_lists.append(dict(name = title, price = price, type=badge, delPrice = delPrice))
+    delPrice = item.find("span", class_="deal-delivery").get_text().strip() #get the delivery price
+    items.append(dict(
+        name = title, price = price, 
+        type = badge, 
+        delPrice = delPrice, 
+        link = "https://arca.live"+site_link))
     #append to item_list with dictionary
 
 #finding ended(or ded) items
@@ -27,16 +32,20 @@ item_ded_list = soup.find_all(class_="vrow-top deal deal-close") #you can see it
 
 for ded in item_ded_list:
     ded_add = ded.find("a", class_ = "title") #get the title
-    ended_lists.append(ded_add.get_text().strip()) #and add it to ended_list
+    items_ended.append(ded_add.get_text().strip()) #and add it to ended_list
 
-is_ded = False #in order to check if the item is ended
-for a in range(len(item_lists)):
-    for b in range(len(ended_lists)):
-        if(item_lists[a]['name'] == ended_lists[b]): #compare the item
-            is_ded = True #it is ded item lol. do not print it
+
+#in order to check if the item is ended
+for b in range(len(items_ended)):
+    for a in range(len(items)):
+        if(items[a]['name'] == items_ended[b]):
+            del items[a]
             break
-    if(not is_ded): #not ded: print it
-        print(item_lists[a]['name'], item_lists[a]['price'], item_lists[a]['type'], item_lists[a]['delPrice'])
+
+''' 
+출력
+for a in range(len(items)):
+    print(items[a]['name'], items[a]['price'], items[a]['type'], items[a]['delPrice'], items[a]['link']) '''
 
 '''
 name: 제품 이름
